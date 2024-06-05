05ac20f59e0781f120297ad105f7f36aed520618
lilpinc <133043171+lilpinc@users.noreply.github.com>
2024-06-05 13:23:47 -0400

    added index.html to base level

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom/client'
+import React from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom/dist'
 
 import Home from './components/Home Page/Home';
