132fe31186bceb2af09c181374773824f2e1d6bd
lilpinc <133043171+lilpinc@users.noreply.github.com>
2024-06-12 15:17:46 -0400

    test 11

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '',
+  root: 'src',
+  build: {
+    outDir: './dist'
+  },
   plugins: [react()],
   server: {
     port: 3000,
